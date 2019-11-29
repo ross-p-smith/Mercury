19ba503d8a1252e53085b49290ebcf15281d91ac
ross-p-smith <rosmith@microsoft.com>
2019-11-29 22:27:35 +0000

    Dont delete KEDA

--- deploy/common/scripts/integrationtest.sh
+++ deploy/common/scripts/integrationtest.sh
@@ -5,9 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Get load balancer ip as endpoint for integration tests to run against 
 API_BASE_ADDRESS=$($DIR/loadbalancerip.sh)
 
-# Need to delete the keda pod because it spins up before AAD pod identity can 
-# be administered to keda. This will run after loadbalancerip to give aadpod
-# identity pod time to create
-kubectl delete pod $(kubectl get pods -o name | grep keda |  sed "s/^.\{4\}//")
-
 docker run -e API_BASE_ADDRESS=$API_BASE_ADDRESS ingestion-api-test:latest
